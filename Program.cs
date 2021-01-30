f9db580b901aa02ffe13a5f43f9dfed57ce7879a
Petr D. Novak <petr-novak-donald@protonmail.com>
2021-01-30 18:19:45 +0100

    upload files

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSubrutineCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vypis5kratText(5, "napis");
+            Console.ReadLine();
+        }
+
+        static void Vypis5kratText(int max, string text)
+        {
+            for (int i = 0; i < max; i++)
+            {
+                Console.WriteLine(text);
+            }
+        }
+
+    }
+}
